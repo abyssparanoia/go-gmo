1fd05d4acca462b016d85864f63da46553fd9b19
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-04-25 21:37:12 +0900

    feat: impl create file for sftp

--- pg/payment/account_transfer.go
+++ pg/payment/account_transfer.go
@@ -1,10 +1,14 @@
 package payment
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"encoding/csv"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/abyssparanoia/go-gmo/internal/validate"
 
@@ -30,8 +34,7 @@ func (atr *AccountTransferRecord) Validate() error {
 	return validate.Struct(atr)
 }
 
-// CreateAccountTranserCSV ... create csv file for account transfer
-func CreateAccountTranserCSV(
+func createAccountTranserFile(
 	fileName string,
 	data []*AccountTransferRecord,
 ) (*os.File, error) {
@@ -48,14 +51,94 @@ func CreateAccountTranserCSV(
 		return gocsv.NewSafeCSVWriter(writer)
 	})
 
-	file, err := os.OpenFile(fmt.Sprintf("%s.csv", fileName), os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	file, err := os.OpenFile(fmt.Sprintf("%s", fileName), os.O_CREATE|os.O_RDWR, 0664)
 	if err != nil {
+		file.Close()
 		return nil, err
 	}
 
 	if err = gocsv.MarshalFile(data, file); err != nil {
+		file.Close()
 		return nil, err
 	}
 
 	return file, nil
+}
+
+// CreateAccountTranserCSV ... create csv file for account transfer
+func CreateAccountTranserCSV(
+	fileName string,
+	data []*AccountTransferRecord,
+) (*os.File, error) {
+	return createAccountTranserFile(fileName, data)
+}
+
+// CreateAccountTranserFileForSFTP ... create tar file for sftp
+func CreateAccountTranserFileForSFTP(
+	shopID string,
+	sequence int,
+	uploadTime time.Time,
+	data []*AccountTransferRecord,
+) (*os.File, error) {
+
+	year := strconv.Itoa(uploadTime.Year())[2:]
+	orgMonth := int(uploadTime.Month())
+	var month string
+	if orgMonth < 10 {
+		month = fmt.Sprintf("0%s", strconv.Itoa(orgMonth))
+	} else {
+		month = strconv.Itoa(orgMonth)
+	}
+	baseFileName := fmt.Sprintf("kfuri%s%s%s", shopID, year, month)
+	txtFileName := fmt.Sprintf("%s.txt", baseFileName)
+	gzFileName := fmt.Sprintf("%s.tar.gz", baseFileName)
+
+	txtFile, err := createAccountTranserFile(txtFileName, data)
+	if err != nil {
+		return nil, err
+	}
+	txtFile.Close()
+
+	txtFile, err = os.Open(txtFileName)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		txtFile.Close()
+		os.Remove(txtFileName)
+	}()
+
+	stat, err := txtFile.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	outputFile, err := os.Create(gzFileName)
+	if err != nil {
+		return nil, err
+	}
+
+	gzw := gzip.NewWriter(outputFile)
+	defer gzw.Close()
+
+	tw := tar.NewWriter(gzw)
+	defer tw.Close()
+
+	hdr := &tar.Header{
+		Name:    txtFileName,
+		Size:    stat.Size(),
+		Mode:    int64(stat.Mode()),
+		ModTime: stat.ModTime(),
+	}
+	if err = tw.WriteHeader(hdr); err != nil {
+		outputFile.Close()
+		return nil, err
+	}
+
+	if _, err = io.Copy(tw, txtFile); err != nil {
+		outputFile.Close()
+		return nil, err
+	}
+
+	return outputFile, nil
 }
