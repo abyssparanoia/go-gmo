edce76f71b2f186339eec29d0619cf0fb87c9d60
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-02 02:32:54 +0900

    feat: impl creditcard client

---
+++ pg/payment/creditcard/client.go
@@ -0,0 +1,122 @@
+package payment
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/cenkalti/backoff"
+)
+
+// Client ... gmo pg payment API client
+type Client struct {
+	HTTPClient *http.Client
+	ShopID     string
+	ShopPass   string
+	APIHost    string
+}
+
+// NewClient ... new client
+func NewClient(
+	shopID,
+	shopPass string,
+	sandBox bool) (*Client, error) {
+	if shopID == "" || shopPass == "" {
+		return nil, errors.New("Not enough parameters")
+	}
+
+	var apiHost string
+	if sandBox {
+		apiHost = apiHostSandbox
+	} else {
+		apiHost = apiHostProduction
+	}
+
+	return &Client{
+		HTTPClient: &http.Client{
+			Timeout: time.Second * 30,
+		},
+		ShopID:   shopID,
+		ShopPass: shopPass,
+		APIHost:  apiHost,
+	}, nil
+}
+
+type baseRequestBody struct {
+	ShopID   string `json:"ShopID"`
+	ShopPass string `json:"ShopPass"`
+}
+
+func (c *Client) do(
+	path string,
+	body interface{},
+	respBody interface{},
+) (*http.Response, error) {
+
+	var reqBody map[string]interface{}
+
+	baseBody, err := json.Marshal(&baseRequestBody{
+		ShopID:   c.ShopID,
+		ShopPass: c.ShopPass,
+	})
+	if err != nil {
+		return nil, err
+	}
+	if err = json.Unmarshal(baseBody, &reqBody); err != nil {
+		return nil, err
+	}
+
+	addtionalBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	if err = json.Unmarshal(addtionalBody, &reqBody); err != nil {
+		return nil, err
+	}
+
+	reqBodyReader, err := json.Marshal(&reqBody)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf("%s/%s", c.APIHost, path),
+		bytes.NewReader(reqBodyReader),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	var resp *http.Response
+	backoffCfg := backoff.WithMaxRetries(backoff.NewExponentialBackOff(), 4)
+	err = backoff.Retry(func() (err error) {
+		resp, err = c.HTTPClient.Do(req)
+		if err != nil {
+			return err
+		}
+		return nil
+	}, backoffCfg)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if respBody != nil {
+		if w, ok := respBody.(io.Writer); ok {
+			io.Copy(w, resp.Body)
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(respBody)
+			if err != nil && err != io.EOF {
+				return nil, err
+			}
+		}
+	}
+
+	return resp, nil
+}
