69c46dee901d811516bf9acc87a9a14ab13a4bcc
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-02 04:21:53 +0900

    feat: added test

--- pg/payment/credit_card_test.go
+++ pg/payment/credit_card_test.go
@@ -34,10 +34,41 @@ func TestSaveCard(t *testing.T) {
 	cli.APIHost = apiHostTest
 	req := &SaveCardRequest{
 		MemberID:     "memberID",
+		SeqMode:      "1",
 		CardNo:       "3131414141414141",
 		Expire:       "0125",
 		SecurityCode: "0000",
 	}
 	result, _ := cli.SaveCard(req)
+	assert.Equal(t, expected, result)
+}
+
+func TestDeleteCard(t *testing.T) {
+
+	expected := &DeleteCardResponse{
+		CardSeq: "0001",
+		ErrCode: "errCode",
+		ErrInfo: "errInfo",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		js, _ := json.Marshal(expected)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+	req := &DeleteCardRequest{
+		MemberID: "memberID",
+		CardSeq:  "0001",
+	}
+	result, _ := cli.DeleteCard(req)
 	assert.Equal(t, expected, result)
 }
