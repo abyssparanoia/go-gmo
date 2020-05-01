f5d0b5267e8d9530a1051fb8e9cf39d367e0484c
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-02 02:55:43 +0900

    feat: impl entry tran

--- pg/payment/creditcard/const.go
+++ pg/payment/creditcard/const.go
@@ -1,34 +1,11 @@
 package creditcard
 
 const (
-	apiHostSandbox                = "https://pt01.mul-pay.jp"
-	apiHostProduction             = "https://p01.mul-pay.jp"
-	apiHostTest                   = "http://p01.mul-pay.jp"
-	entryBankAccountPath          = "payment/BankAccountEntry.idPass"
-	getResultEntryBankAccountPath = "payment/BankAccountTranResult.idPass"
+	apiHostSandbox    = "https://pt01.mul-pay.jp"
+	apiHostProduction = "https://p01.mul-pay.jp"
+	apiHostTest       = "http://p01.mul-pay.jp"
+	entryTranPath     = "payment/EntryTran.idPass"
 )
-
-// ResultEntryBankAccountStatus ... entry bank account status
-type ResultEntryBankAccountStatus string
-
-const (
-	// ResultEntryBankAccountStatusEntry ... entry complete
-	ResultEntryBankAccountStatusEntry ResultEntryBankAccountStatus = "ENTRY"
-	// ResultEntryBankAccountStatusStart ... entry start
-	ResultEntryBankAccountStatusStart ResultEntryBankAccountStatus = "START"
-	// ResultEntryBankAccountStatusTerm ... result confirm
-	ResultEntryBankAccountStatusTerm ResultEntryBankAccountStatus = "TERM"
-	// ResultEntryBankAccountStatusSuccess ... entry success
-	ResultEntryBankAccountStatusSuccess ResultEntryBankAccountStatus = "SUCCESS"
-	// ResultEntryBankAccountStatusFail ... entry fail because of bank reason
-	ResultEntryBankAccountStatusFail ResultEntryBankAccountStatus = "FAIL"
-	// ResultEntryBankAccountStatusUnprocessed ... entry fail because of other reason
-	ResultEntryBankAccountStatusUnprocessed ResultEntryBankAccountStatus = "UNPROCESSED"
-)
-
-func (s ResultEntryBankAccountStatus) String() string {
-	return string(s)
-}
 
 // JobCD ... job cd type
 type JobCD string
