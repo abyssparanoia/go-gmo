050aaa79325ba78eafa5bca8a0a4b0e9965242e4
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-02 03:38:53 +0900

    feat: added test

--- pg/payment/member_test.go
+++ pg/payment/member_test.go
@@ -39,3 +39,62 @@ func TestSaveMember(t *testing.T) {
 	result, _ := cli.SaveMember(req)
 	assert.Equal(t, expected, result)
 }
+
+func TestUpdateMember(t *testing.T) {
+
+	expected := &UpdateMemberResponse{
+		MemberID: "memberID",
+		ErrCode:  "errCode",
+		ErrInfo:  "errInfo",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		js, _ := json.Marshal(expected)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+	req := &UpdateMemberRequest{
+		MemberID:   "memberID",
+		MemberName: "memberName",
+	}
+	result, _ := cli.UpdateMember(req)
+	assert.Equal(t, expected, result)
+}
+
+func TestDeleteMember(t *testing.T) {
+
+	expected := &DeleteMemberResponse{
+		MemberID: "memberID",
+		ErrCode:  "errCode",
+		ErrInfo:  "errInfo",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		js, _ := json.Marshal(expected)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+	req := &DeleteMemberRequest{
+		MemberID: "memberID",
+	}
+	result, _ := cli.DeleteMember(req)
+	assert.Equal(t, expected, result)
+}
