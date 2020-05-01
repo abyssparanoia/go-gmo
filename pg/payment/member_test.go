94cc71a2461b04fc0547334a646728aaffed7688
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-02 03:45:05 +0900

    feat: added test

--- pg/payment/member_test.go
+++ pg/payment/member_test.go
@@ -98,3 +98,34 @@ func TestDeleteMember(t *testing.T) {
 	result, _ := cli.DeleteMember(req)
 	assert.Equal(t, expected, result)
 }
+
+func TestSearchMember(t *testing.T) {
+
+	expected := &SearchMemberResponse{
+		MemberID:   "memberID",
+		MemberName: "memberName",
+		DeleteFlag: "0",
+		ErrCode:    "errCode",
+		ErrInfo:    "errInfo",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		js, _ := json.Marshal(expected)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+	req := &SearchMemberRequest{
+		MemberID: "memberID",
+	}
+	result, _ := cli.SearchMember(req)
+	assert.Equal(t, expected, result)
+}
