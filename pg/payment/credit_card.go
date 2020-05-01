2557e5760010025503815febac7015a698f97b88
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-02 08:22:47 +0900

    feat: impl search card

--- pg/payment/credit_card.go
+++ pg/payment/credit_card.go
@@ -1,6 +1,9 @@
 package payment
 
-import "github.com/abyssparanoia/go-gmo/internal/pkg/validate"
+import (
+	"github.com/abyssparanoia/go-gmo/internal/pkg/parser"
+	"github.com/abyssparanoia/go-gmo/internal/pkg/validate"
+)
 
 // SaveCardRequest ... save card request
 type SaveCardRequest struct {
@@ -85,4 +88,76 @@ func (cli *Client) DeleteCard(
 		return nil, err
 	}
 	return res, nil
+}
+
+// SearchCardRequest ...
+type SearchCardRequest struct {
+	MemberID        string `schema:"MemberID" validate:"required,max=60"`
+	SeqMode         string `schema:"SeqMode"`
+	CardSeq         string `schema:"CardSeq"`
+	UseFloatingMask string `schema:"UseFloatingMask"`
+}
+
+// Validate ... validate
+func (r *SearchCardRequest) Validate() error {
+	return validate.Struct(r)
+}
+
+// SearchCardResponseDetail ... detail
+type SearchCardResponseDetail struct {
+	CardSeq                string `schema:"CardSeq"`
+	DefaultFlag            string `schema:"DefaultFlag"`
+	CardName               string `schema:"CardName"`
+	CardNo                 string `schema:"CardNo"`
+	Expire                 string `schema:"Expire"`
+	HolderName             string `schema:"HolderName"`
+	DeleteFlag             string `schema:"DeleteFlag"`
+	ErrCode                string `schema:"ErrCode"`
+	ErrInfo                string `schema:"ErrInfo"`
+	Brand                  string `schema:"Brand"`
+	DomesticFlag           string `schema:"DomesticFlag"`
+	IssuerCode             string `schema:"IssuerCode"`
+	DebitPrepaidFlag       string `schema:"DebitPrepaidFlag"`
+	DebitPrepaidIssuerName string `schema:"DebitPrepaidIssuerName"`
+	ForwardFinal           string `schema:"ForwardFinal"`
+}
+
+// SearchCardResponse ... search card response
+type SearchCardResponse struct {
+	Cards   []*SearchCardResponseDetail
+	ErrCode string
+	ErrInfo string
+}
+
+// SearchCard ... search card
+func (cli *Client) SearchCard(
+	req *SearchCardRequest,
+) (*SearchCardResponse, error) {
+
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+	res := SearchCardResponseDetail{}
+	_, err := cli.do(searchCardPath, req, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	parsedResList := parser.ParseToMultiObject(res)
+
+	var convertRes *SearchCardResponse
+	convertRes.Cards = make([]*SearchCardResponseDetail, len(parsedResList))
+	for _, parsedRes := range parsedResList {
+		var dst SearchCardResponseDetail
+		err = parser.MapToStruct(parsedRes, &dst)
+		if err != nil {
+			return nil, err
+		}
+		convertRes.Cards = append(convertRes.Cards, &dst)
+	}
+
+	convertRes.ErrCode = res.ErrCode
+	convertRes.ErrInfo = res.ErrInfo
+
+	return convertRes, nil
 }
