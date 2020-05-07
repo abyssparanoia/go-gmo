bde5d270a28015111e8308c9528ae82733fc59d5
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-08 07:49:28 +0900

    fix: handling

--- internal/pkg/parser/parser.go
+++ internal/pkg/parser/parser.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"reflect"
 	"strings"
@@ -14,12 +15,9 @@ const errInfo string = "ErrInfo"
 func ParseToMultiObject(dst interface{}) []map[string]interface{} {
 	values := reflect.ValueOf(dst)
 	fields := reflect.TypeOf(dst)
-
 	var length int
-
 	tempMap := make(map[string][]string)
 	var fieldNames []string
-
 	for i := 0; i < values.NumField(); i++ {
 		v := values.Field(i).Interface()
 		f := fields.Field(i).Name
@@ -35,22 +33,34 @@ func ParseToMultiObject(dst interface{}) []map[string]interface{} {
 	}
 
 	var multiRes []map[string]interface{}
-
 	for i := 0; i < length; i++ {
-
 		singleRes := make(map[string]interface{})
-
 		for g := 0; g < len(fieldNames); g++ {
 			singleRes[fieldNames[g]] = tempMap[fieldNames[g]][i]
 		}
-
 		multiRes = append(multiRes, singleRes)
 	}
-
 	return multiRes
 }
 
-// MapToStruct ... map to struct
+// ParseError ... parse error
+func ParseError(dst interface{}) error {
+	dstMap := StructToMap(dst)
+	errInfoField, ok := dstMap[errInfo]
+	if !ok {
+		panic("no error info field")
+	}
+	errInfoFieldStr, ok := errInfoField.(string)
+	if !ok {
+		panic("invalid type")
+	}
+	if errInfoField == "" {
+		return nil
+	}
+	return errors.New(errInfoFieldStr)
+}
+
+// MapToStruct ... convert map to struct
 func MapToStruct(m map[string]interface{}, val interface{}) error {
 	tmp, err := json.Marshal(m)
 	if err != nil {
@@ -61,4 +71,19 @@ func MapToStruct(m map[string]interface{}, val interface{}) error {
 		return err
 	}
 	return nil
+}
+
+// StructToMap ... convert struct to map
+func StructToMap(data interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+	elem := reflect.ValueOf(data).Elem()
+	size := elem.NumField()
+
+	for i := 0; i < size; i++ {
+		field := elem.Type().Field(i).Name
+		value := elem.Field(i).Interface()
+		result[field] = value
+	}
+
+	return result
 }
