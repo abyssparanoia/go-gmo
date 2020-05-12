af60a1544c14838ab2ec42306505b73a0aeaffef
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-13 07:46:22 +0900

    feat: impl new client for example

---
+++ example/payment/example_util.go
@@ -0,0 +1,25 @@
+package payment
+
+import (
+	"github.com/abyssparanoia/go-gmo/pg/payment"
+	"github.com/caarlos0/env/v6"
+)
+
+type environment struct {
+	SiteID   string `env:"SITE_ID,required"`
+	SitePass string `env:"SITE_PASSWORD,required"`
+	ShopID   string `env:"SHOP_ID,required"`
+	ShopPass string `env:"SHOP_PASSWORD,required"`
+}
+
+func newClient() *payment.Client {
+	e := &environment{}
+	if err := env.Parse(e); err != nil {
+		panic(err)
+	}
+	cli, err := payment.NewClient(e.SiteID, e.SitePass, e.ShopID, e.ShopPass, true)
+	if err != nil {
+		panic(err)
+	}
+	return cli
+}
