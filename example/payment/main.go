0ecada7593b6e4cbf2d0612bff5e325fafd9c752
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-05-18 13:09:39 +0900

    feat: added exec

--- example/payment/main.go
+++ example/payment/main.go
@@ -8,8 +8,11 @@ import (
 
 func main() {
 	cli := newClient()
+
+	orderID := "orderID5"
+
 	req1 := &payment.EntryTranGANBRequest{
-		OrderID: "orderID3",
+		OrderID: orderID,
 		Amount:  2000,
 	}
 	result1, err := cli.EntryTranGANB(req1)
@@ -19,14 +22,26 @@ func main() {
 	}
 	fmt.Printf("%+v", result1)
 
-	req2 := &payment.SearchTradeMultiRequest{
-		OrderID: "orderID3",
-		PayType: "36",
+	req2 := &payment.ExecTranGANBRequest{
+		OrderID:    orderID,
+		AccessID:   result1.AccessID,
+		AccessPass: result1.AccessPass,
 	}
-	result2, err := cli.SearchTradeMulti(req2)
+	result2, err := cli.ExecTranGANB(req2)
 	if err != nil {
 		fmt.Printf("%+v", err)
 		panic(err)
 	}
 	fmt.Printf("%+v", result2)
+
+	req3 := &payment.SearchTradeMultiRequest{
+		OrderID: orderID,
+		PayType: "36",
+	}
+	result3, err := cli.SearchTradeMulti(req3)
+	if err != nil {
+		fmt.Printf("%+v", err)
+		panic(err)
+	}
+	fmt.Printf("%+v", result3)
 }
