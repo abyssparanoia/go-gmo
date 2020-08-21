221008f0002503bc17dd8022b6f905a903e43277
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2020-08-21 16:14:26 +0900

    feat: added test code

--- payment/tran_test.go
+++ payment/tran_test.go
@@ -76,3 +76,35 @@ func TestExecTran(t *testing.T) {
 	result, _ := cli.ExecTran(req)
 	assert.Equal(t, expected, result)
 }
+
+func TestAlterTran(t *testing.T) {
+
+	expected := &AlterTranResponse{
+		AccessID:   "accessID",
+		AccessPass: "accessPass",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		form := url.Values{}
+		_ = parser.Encoder().Encode(expected, form)
+		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+		w.Write([]byte(form.Encode()))
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+	req := &AlterTranRequest{
+		AccessID:   "accessID",
+		AccessPass: "accessPass",
+		JobCD:      JobCDCapture,
+		Amount:     1000,
+	}
+	result, _ := cli.AlterTran(req)
+	assert.Equal(t, expected, result)
+}
