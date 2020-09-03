2cbd414371427d667fe037e9a96f52da9b001440
0daryo <plm23.ccc@gmail.com>
2020-09-03 18:15:40 +0900

    feat: omitempty

--- deferred/modification.go
+++ deferred/modification.go
@@ -9,6 +9,62 @@ type modifyRequest struct {
 	KindInfo   *kindInfo          `xml:"kindInfo" validate:"required"`
 }
 
+type buyerModification struct {
+	GMOTransactionID  string `xml:"gmoTransactionId" validate:"required"`
+	ShopTransactionID string `xml:"shopTransactionId,omitempty"`
+	ShopOrderDate     string `xml:"shopOrderDate,omitempty"`
+	FullName          string `xml:"fullName,omitempty"`
+	FullNameKana      string `xml:"fullKanaName,omitempty"`
+	ZipCode           string `xml:"zipCode,omitempty"`
+	Address           string `xml:"address,omitempty"`
+	CompanyName       string `xml:"companyName,omitempty"`
+	DepartmentName    string `xml:"departmentName,omitempty"`
+	Tel1              string `xml:"tel1,omitempty"`
+	Tel2              string `xml:"tel2,omitempty"`
+	Email             string `xml:"email1,omitempty"`
+	Email2            string `xml:"email2,omitempty"`
+	BilledAmount      string `xml:"billedAmount,omitempty"`
+	GMOExtend1        string `xml:"gmoExtend1,omitempty"`
+	PaymentType       string `xml:"paymentType,omitempty"`
+	Sex               string `xml:"sex,omitempty"`
+	BirthDay          string `xml:"birthDay,omitempty"`
+	MemberRegistDate  string `xml:"memberRegistDate,omitempty"`
+	BuyCount          string `xml:"buyCount,omitempty"`
+	BuyAmountTotal    string `xml:"buyAmoutTotal,omitempty"`
+	MemberID          string `xml:"memberId,omitempty"`
+}
+
+func (o *Buyer) toParamModification() *buyerModification {
+	if o == nil {
+		return nil
+	}
+	p := &buyerModification{
+		GMOTransactionID:  o.GMOTransactionID,
+		ShopTransactionID: o.ShopTransactionID,
+		ShopOrderDate:     o.ShopOrderDate,
+		FullName:          o.FullName,
+		FullNameKana:      o.FullNameKana,
+		ZipCode:           o.ZipCode,
+		Address:           o.Address,
+		CompanyName:       o.CompanyName,
+		DepartmentName:    o.DepartmentName,
+		Tel1:              o.Tel1,
+		Tel2:              o.Tel2,
+		Email:             o.Email,
+		Email2:            o.Email2,
+		BilledAmount:      o.BilledAmount,
+		GMOExtend1:        o.GMOExtend1,
+		PaymentType:       o.PaymentType,
+		Sex:               o.Sex,
+		BirthDay:          o.BirthDay,
+		MemberRegistDate:  o.MemberRegistDate,
+		BuyCount:          o.BuyCount,
+		BuyAmountTotal:    o.BuyAmountTotal,
+		MemberID:          o.MemberID,
+	}
+	return p
+}
+
 type modifyResponse struct {
 	Result            string             `xml:"result"`
 	Errors            *gmoErrors         `xml:"errors"`
