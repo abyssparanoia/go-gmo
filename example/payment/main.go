eebc187782ae7baa371f70ad55515f4b9b9a3f7b
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2021-04-10 07:45:49 +0900

    fix: example

--- example/payment/main.go
+++ example/payment/main.go
@@ -3,15 +3,12 @@ package main
 import (
 	"fmt"
 
-	"golang.org/x/text/encoding/japanese"
-	"golang.org/x/text/transform"
-
 	"github.com/abyssparanoia/go-gmo/payment"
 )
 
 func main() {
 	cli := newClient()
-	orderID := "orderID5"
+	orderID := "orderID6"
 
 	req1 := &payment.EntryTranGANBRequest{
 		OrderID: orderID,
@@ -48,10 +45,4 @@ func main() {
 	fmt.Printf("%+v", result3)
 	fmt.Println(result3.GanbBankName)
 
-	str, _, err := transform.String(japanese.ShiftJIS.NewDecoder(), result3.GanbBankName)
-	if err != nil {
-		fmt.Printf("%+v", err)
-		panic(err)
-	}
-	fmt.Println(str)
 }
