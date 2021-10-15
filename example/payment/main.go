37b93d4c49780d825b0af97af332e3d5bc39bb8f
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2021-10-15 23:27:55 +0900

    fix: main script

--- example/payment/main.go
+++ example/payment/main.go
@@ -8,41 +8,78 @@ import (
 
 func main() {
 	cli := newClient()
-	orderID := "orderID6"
+	// orderID := "orderID6"
 
-	req1 := &payment.EntryTranGANBRequest{
-		OrderID: orderID,
-		Amount:  2000,
+	// req1 := &payment.EntryTranGANBRequest{
+	// 	OrderID: orderID,
+	// 	Amount:  2000,
+	// }
+	// result1, err := cli.EntryTranGANB(req1)
+	// if err != nil {
+	// 	fmt.Printf("%+v", err)
+	// 	panic(err)
+	// }
+	// fmt.Printf("%+v", result1)
+
+	// req2 := &payment.ExecTranGANBRequest{
+	// 	OrderID:    orderID,
+	// 	AccessID:   result1.AccessID,
+	// 	AccessPass: result1.AccessPass,
+	// }
+	// result2, err := cli.ExecTranGANB(req2)
+	// if err != nil {
+	// 	fmt.Printf("%+v", err)
+	// 	panic(err)
+	// }
+	// fmt.Printf("%+v", result2)
+
+	// req3 := &payment.SearchTradeMultiRequest{
+	// 	OrderID: orderID,
+	// 	PayType: "36",
+	// }
+	// result3, err := cli.SearchTradeMulti(req3)
+	// if err != nil {
+	// 	fmt.Printf("%+v", err)
+	// 	panic(err)
+	// }
+	// fmt.Printf("%+v", result3)
+	// fmt.Println(result3.GanbBankName)
+
+	sameMemberReq := &payment.SaveMemberRequest{
+		MemberID:   "aadwaddwadwadwada2",
+		MemberName: "田中太郎",
 	}
-	result1, err := cli.EntryTranGANB(req1)
+
+	saveMemberRes, err := cli.SaveMember(sameMemberReq)
 	if err != nil {
 		fmt.Printf("%+v", err)
 		panic(err)
 	}
-	fmt.Printf("%+v", result1)
 
-	req2 := &payment.ExecTranGANBRequest{
-		OrderID:    orderID,
-		AccessID:   result1.AccessID,
-		AccessPass: result1.AccessPass,
+	fmt.Printf("%+v", saveMemberRes)
+
+	deleteMemberReq := &payment.DeleteMemberRequest{
+		MemberID: sameMemberReq.MemberID,
 	}
-	result2, err := cli.ExecTranGANB(req2)
+
+	deleteMemberRes, err := cli.DeleteMember(deleteMemberReq)
 	if err != nil {
 		fmt.Printf("%+v", err)
 		panic(err)
 	}
-	fmt.Printf("%+v", result2)
 
-	req3 := &payment.SearchTradeMultiRequest{
-		OrderID: orderID,
-		PayType: "36",
+	fmt.Printf("%+v", deleteMemberRes)
+
+	sameMemberReq2 := &payment.SaveMemberRequest{
+		MemberID:   sameMemberReq.MemberID,
+		MemberName: "田中太郎",
 	}
-	result3, err := cli.SearchTradeMulti(req3)
+
+	saveMemberRes2, err := cli.SaveMember(sameMemberReq2)
 	if err != nil {
 		fmt.Printf("%+v", err)
 		panic(err)
 	}
-	fmt.Printf("%+v", result3)
-	fmt.Println(result3.GanbBankName)
 
+	fmt.Printf("%+v", saveMemberRes2)
 }
