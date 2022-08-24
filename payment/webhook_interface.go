b6cdd9d14fc4ff03af4c1783711c42c4b19f6c48
Shun Suga <49981407+dionomusuko@users.noreply.github.com>
2022-08-24 21:26:30 +0900

    feat: payment slip

--- payment/webhook_interface.go
+++ payment/webhook_interface.go
@@ -14,3 +14,32 @@ type WebhookResultEntryBankAccountRequest struct {
 	ErrDetail             string                       `schema:"ErrDetail"`
 	AccountIdentification string                       `schema:"AccountIdentification"`
 }
+
+// WebhookResultPaymentSlipRequest ... webhook request parameter
+type WebhookResultPaymentSlipRequest struct {
+	ShopID      string                         `shema:"ShopID"`
+	ShopPass    string                         `shema:"ShopPass"`
+	AccessID    string                         `schema:"AccessID"`
+	AccessPass  string                         `schema:"AccessPass"`
+	OrderID     string                         `schema:"OrderID"`
+	Status      WebhookResultPaymentSlipStatus `schema:"Status"`
+	JobCd       JobCD                          `schema:"JobCd"`
+	Amount      int                            `schema:"Amount"`
+	Tax         int                            `schema:"Tax"`
+	Currency    string                         `schema:"Currency"`
+	Forward     string                         `schema:"Forward"`
+	Method      int                            `schema:"Method"`
+	PayTimes    int                            `schema:"PayTimes"`
+	TranID      string                         `schema:"TranID"`
+	Approve     int                            `schema:"Approve"`
+	TranDate    string                         `schema:"TranDate"`
+	PaymentTerm int                            `schema:"PaymentTerm"`
+	ErrCode     string                         `schema:"ErrCode"`
+	ErrDetail   string                         `schema:"ErrDetail"`
+	PayType     PayType                        `schema:"PayType"`
+}
+
+// WebhookResultResponse ... webhook response parameter
+type WebhookResultResponse struct {
+	Status WebhookResultResponseStatus
+}
