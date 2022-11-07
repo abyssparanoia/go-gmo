015d3925b5f7bda0eb2cd7cd2d506f7733455449
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-11-08 01:22:23 +0900

    feat: impl mail deposit registration

---
+++ remittance/const.go
@@ -0,0 +1,8 @@
+package remittance
+
+const (
+	apiHostSandbox              = "https://test-remittance.gmopg.jp"
+	apiHostProduction           = "https://remittance.gmopg.jp"
+	apiHostTest                 = "http://remittance.gmopg.jp"
+	mailDepositRegistrationPath = "api/shop/MailDepositRegistration.json"
+)
