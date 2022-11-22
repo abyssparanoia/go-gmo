147f2dd20f9cc39bd941ebe7f4c075a17542e8d5
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2022-11-22 13:32:28 +0900

    feat: impl named type

--- remittance/mail_deposit_registration.go
+++ remittance/mail_deposit_registration.go
@@ -6,26 +6,26 @@ import (
 )
 
 type MailDepositRegistrationRequest struct {
-	Method                 string `json:"Method" validate:"required"`
-	DepositID              string `json:"Deposit_ID" validate:"required,max=27"`
-	Amount                 int    `json:"Amount" validate:"required"`
-	MailAddress            string `json:"Mail_Address" validate:"required,email,max=200"`
-	ShopMailAddress        string `json:"Shop_Mail_Address,omitempty"`
-	MailDepositAccountName string `json:"Mail_Deposit_Account_Name,omitempty"`
-	Expire                 string `json:"Expire,omitempty"`
-	AuthCode               string `json:"Auth_Code,omitempty"`
-	AuthCode2              string `json:"Auth_Code2,omitempty"`
-	AuthCode3              string `json:"Auth_Code3,omitempty"`
-	RemitMethodBank        string `json:"Remit_Method_Bank,omitempty"`
-	RemitMethodSevenatm    string `json:"Remit_Method_Sevenatm,omitempty"`
-	SevenatmPaymentTermDay int    `json:"Sevenatm_Payment_Term_Day,omitempty"`
-	RemitMethodAmazongift  string `json:"Remit_Method_Amazongift,omitempty"`
-	RemitMethodAupay       string `json:"Remit_Method_Aupay,omitempty"`
-	MailTemplateFree1      string `json:"Mail_Template_Free1,omitempty"`
-	MailTemplateFree2      string `json:"Mail_Template_Free2,omitempty"`
-	MailTemplateFree3      string `json:"Mail_Template_Free3,omitempty"`
-	MailTemplateNumber     int    `json:"Mail_Template_Number,omitempty"`
-	BankID                 string `json:"Bank_ID,omitempty"`
+	Method                 Method                  `json:"Method" validate:"required"`
+	DepositID              string                  `json:"Deposit_ID" validate:"required,max=27"`
+	Amount                 int                     `json:"Amount" validate:"required"`
+	MailAddress            string                  `json:"Mail_Address" validate:"required,email,max=200"`
+	ShopMailAddress        string                  `json:"Shop_Mail_Address,omitempty"`
+	MailDepositAccountName string                  `json:"Mail_Deposit_Account_Name,omitempty"`
+	Expire                 string                  `json:"Expire,omitempty"`
+	AuthCode               string                  `json:"Auth_Code,omitempty"`
+	AuthCode2              string                  `json:"Auth_Code2,omitempty"`
+	AuthCode3              string                  `json:"Auth_Code3,omitempty"`
+	RemitMethodBank        SelectablePaymentMethod `json:"Remit_Method_Bank,omitempty"`
+	RemitMethodSevenatm    SelectablePaymentMethod `json:"Remit_Method_Sevenatm,omitempty"`
+	SevenatmPaymentTermDay int                     `json:"Sevenatm_Payment_Term_Day,omitempty"`
+	RemitMethodAmazongift  SelectablePaymentMethod `json:"Remit_Method_Amazongift,omitempty"`
+	RemitMethodAupay       SelectablePaymentMethod `json:"Remit_Method_Aupay,omitempty"`
+	MailTemplateFree1      string                  `json:"Mail_Template_Free1,omitempty"`
+	MailTemplateFree2      string                  `json:"Mail_Template_Free2,omitempty"`
+	MailTemplateFree3      string                  `json:"Mail_Template_Free3,omitempty"`
+	MailTemplateNumber     int                     `json:"Mail_Template_Number,omitempty"`
+	BankID                 string                  `json:"Bank_ID,omitempty"`
 }
 
 func (r *MailDepositRegistrationRequest) Validate() error {
