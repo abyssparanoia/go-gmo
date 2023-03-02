e5a164ea4b97624c34da50ae1a1dd8622f1fdd5a
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-03-02 19:34:07 +0900

    feat: impl cancel retrun

--- payment/paypay_test.go
+++ payment/paypay_test.go
@@ -103,3 +103,34 @@ func TestPayPaySales(t *testing.T) {
 	result, _ := cli.PayPaySales(req)
 	assert.Equal(t, expected, result)
 }
+
+func TestPayPayCancelReturn(t *testing.T) {
+
+	expected := &PayPayCancelReturnResponse{
+		OrderID: "orderID",
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		form := url.Values{}
+		_ = parser.Encoder().Encode(expected, form)
+		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+		w.Write([]byte(form.Encode()))
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+
+	req := &PayPayCancelReturnRequest{
+		AccessID:     "accessID",
+		AccessPass:   "accessPass",
+		OrderID:      "orderID",
+		CancelAmount: 1000,
+	}
+	result, _ := cli.PayPayCancelReturn(req)
+	assert.Equal(t, expected, result)
+}
