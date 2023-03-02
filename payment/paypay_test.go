7c6d92711b351ae48ab19eb8fcae0d4192169ca4
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-03-02 16:48:39 +0900

    feat: impl paypay exec tran

--- payment/paypay_test.go
+++ payment/paypay_test.go
@@ -41,3 +41,34 @@ func TestPayPayEntryTran(t *testing.T) {
 	result, _ := cli.PayPayEntryTran(req)
 	assert.Equal(t, expected, result)
 }
+
+func TestPayPayExecTran(t *testing.T) {
+
+	expected := &PayPayExecTranResponse{
+		OrderID: "orderID",
+	}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		form := url.Values{}
+		_ = parser.Encoder().Encode(expected, form)
+		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+		w.Write([]byte(form.Encode()))
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+
+	req := &PayPayExecTranRequest{
+		AccessID:   "accessID",
+		AccessPass: "accessPass",
+		OrderID:    "orderID",
+		RetURL:     "http://localhost:8000/paypay/callback",
+	}
+	result, _ := cli.PayPayExecTran(req)
+	assert.Equal(t, expected, result)
+}
