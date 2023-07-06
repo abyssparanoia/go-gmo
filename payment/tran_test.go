0efb8deeff4a648779a7ecc9faf0469eaeb01a46
Kazunori Ono <matopenkw@gmail.com>
2023-07-06 17:57:25 +0900

    Fix payment

--- payment/tran_test.go
+++ payment/tran_test.go
@@ -77,6 +77,39 @@ func TestExecTran(t *testing.T) {
 	assert.Equal(t, expected, result)
 }
 
+func TestExecTranWithToken(t *testing.T) {
+
+	expected := &ExecTranResponse{
+		ACS:    "acs",
+		ACSUrl: "acs_url",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		form := url.Values{}
+		_ = parser.Encoder().Encode(expected, form)
+		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+		w.Write([]byte(form.Encode()))
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+	req := &ExecTranRequestWithToken{
+		AccessID:   "accessID",
+		AccessPass: "accessPass",
+		OrderID:    "orderID",
+		TokenType:  "1",
+		Token:      "token",
+	}
+	result, _ := cli.ExecTranWithToken(req)
+	assert.Equal(t, expected, result)
+}
+
 func TestAlterTran(t *testing.T) {
 
 	expected := &AlterTranResponse{
