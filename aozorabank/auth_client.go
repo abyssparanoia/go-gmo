689d452da6256d5702d02492b315fee2105bf071
Kazunori Ono <matopenkw@gmail.com>
2023-10-02 11:19:21 +0900

    fix test error

--- aozorabank/auth_client.go
+++ aozorabank/auth_client.go
@@ -185,7 +185,6 @@ func (c *AuthClient) unmarshalError(bodyBytes []byte) error {
 		if err := json.Unmarshal(bodyBytes, errResp); err != nil {
 			return fmt.Errorf("failed to unmarshal error response, bodyBytes=%s,  err=%w", string(bodyBytes), err)
 		}
-		fmt.Println(string(bodyBytes))
 		if errResp.ErrorCode == "" {
 			return fmt.Errorf("failed to unmarshal error response, bodyBytes=%s", string(bodyBytes))
 		}
