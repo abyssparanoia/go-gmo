167c5a5d545bbd50e81c6e38ced0c64ba55fff85
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-03-25 00:45:38 +0200

    feat: add test

---
+++ payment/postpay_test.go
@@ -0,0 +1,42 @@
+package payment
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/abyssparanoia/go-gmo/internal/pkg/parser"
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestPostpayEntryTran(t *testing.T) {
+
+	expected := &PostpayEntryTranResponse{
+		AccessID:   "accessID",
+		AccessPass: "accessPass",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		form := url.Values{}
+		_ = parser.Encoder().Encode(expected, form)
+		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+		w.Write([]byte(form.Encode()))
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+	req := &PostpayEntryTranRequest{
+		OrderID: "orderID",
+		Amount:  4214144,
+		Tax:     2414,
+	}
+	result, _ := cli.PostpayEntryTran(req)
+	assert.Equal(t, expected, result)
+}
