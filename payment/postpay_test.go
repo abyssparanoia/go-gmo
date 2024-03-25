40b842e0fdc3019c114ab3e8ee37eb2c4c599541
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-03-25 02:09:13 +0200

    feat: add test

--- payment/postpay_test.go
+++ payment/postpay_test.go
@@ -40,3 +40,47 @@ func TestPostpayEntryTran(t *testing.T) {
 	result, _ := cli.PostpayEntryTran(req)
 	assert.Equal(t, expected, result)
 }
+
+func TestPostpayExecTran(t *testing.T) {
+
+	expected := &PostpayExecTranResponse{
+		OrderID: "orderID",
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		form := url.Values{}
+		_ = parser.Encoder().Encode(expected, form)
+		w.Header().Set("Content-Type", "application/x-www-form-urlencoded")
+		w.Write([]byte(form.Encode()))
+	}))
+	defer ts.Close()
+	defaultProxy := http.DefaultTransport.(*http.Transport).Proxy
+	http.DefaultTransport.(*http.Transport).Proxy = func(req *http.Request) (*url.URL, error) {
+		return url.Parse(ts.URL)
+	}
+	defer func() { http.DefaultTransport.(*http.Transport).Proxy = defaultProxy }()
+
+	cli, _ := NewClient("siteID", "sitePass", "shopID", "shopPass", false)
+	cli.APIHost = apiHostTest
+	req := &PostpayExecTranRequest{
+		AccessID:            "accessID",
+		AccessPass:          "accessPass",
+		OrderID:             "orderID",
+		CustomerOrderDate:   "20201224",
+		CustomerName:        "決済太郎",
+		CustomerNameKana:    "ケッサイタロウ",
+		CustomerZipCode:     "1000000",
+		CustomerAddress:     "東京都",
+		CustomerTel1:        "09011111111",
+		CustomerEmail1:      "sample1@test.com",
+		CustomerPaymentType: PostpayCustomerPaymentTypeInvoiceSentSeparately,
+		CustomerID:          "testid",
+		DetailName:          "商品名",
+		DetailPrice:         1000,
+		DetailQuantity:      1,
+		DetailBrand:         "ブランド",
+		DetailCategory:      "カテゴリ",
+	}
+	result, _ := cli.PostpayExecTran(req)
+	assert.Equal(t, expected, result)
+}
