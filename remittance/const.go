7597d1ea82f413d7900eb3cda8d7bb5799906fb2
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-06-22 08:01:33 +0300

    feat: impl deposit search

--- remittance/const.go
+++ remittance/const.go
@@ -8,6 +8,7 @@ const (
 	accountRegistrationPath     = "api/AccountRegistration.json"
 	depositRegistrationPath     = "api/DepositRegistration.json"
 	realDepositRegistrationPath = "api/RealDepositRegistration.json"
+	depositSearchPath           = "api/DepositSearch.json"
 )
 
 type MailDepositMethod string
@@ -44,4 +45,27 @@ type DepositResult string
 const (
 	DepositResultCompleted DepositResult = "1"
 	DepositResultFailed    DepositResult = "4"
+)
+
+type DepositBankStatus string
+
+const (
+	DepositBankStatusRegistered     DepositBankStatus = "0"
+	DepositBankStatusCreated        DepositBankStatus = "1"
+	DepositBankStatusFailedCreating DepositBankStatus = "2"
+	DepositBankStatusSent           DepositBankStatus = "3"
+	DepositBankStatusFailedSending  DepositBankStatus = "4"
+	DepositBankStatusCanceled       DepositBankStatus = "9"
+)
+
+type DepositBankResultDetail string
+
+const (
+	DepositBankResultDetailNoAccount             DepositBankResultDetail = "01"
+	DepositBankResultDetailNameMismatch          DepositBankResultDetail = "02"
+	DepositBankResultDetailAccountNumberMismatch DepositBankResultDetail = "03"
+	DepositBankResultDetailSubjectMismatch       DepositBankResultDetail = "04"
+	DepositBankResultDetailAccountClosed         DepositBankResultDetail = "05"
+	DepositBankResultDetailAcceptTimeError       DepositBankResultDetail = "06"
+	DepositBankResultDetailOther                 DepositBankResultDetail = "09"
 )
