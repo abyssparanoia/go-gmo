0b231e96e3a2326611930973f70e3f846a42690f
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2024-11-14 19:58:51 +0900

    feat: impl postpay change api

--- payment/postpay.go
+++ payment/postpay.go
@@ -159,6 +159,89 @@ func (cli *Client) PostpayShippedTran(
 	return res, nil
 }
 
+type PostpayChangeTranRequest struct {
+	AccessID                 string                     `schema:"AccessID" validate:"required"`
+	AccessPass               string                     `schema:"AccessPass" validate:"required"`
+	OrderID                  string                     `schema:"OrderID" validate:"required"`
+	Amount                   int                        `schema:"Amount" validate:"required"`
+	HTTPHeaderAccept         string                     `schema:"HttpHeaderAccept,omitempty" validate:"omitempty"`
+	HTTPHeaderAcceptCharset  string                     `schema:"HttpHeaderAcceptCharset,omitempty" validate:"omitempty"`
+	HTTPHeaderAcceptEncoding string                     `schema:"HttpHeaderAcceptEncoding,omitempty" validate:"omitempty"`
+	HTTPHeaderAcceptLanguage string                     `schema:"HttpHeaderAcceptLanguage,omitempty" validate:"omitempty"`
+	HTTPHeaderClientIp       string                     `schema:"HttpHeaderClientIp,omitempty" validate:"omitempty"`
+	HTTPHeaderConnection     string                     `schema:"HttpHeaderConnection,omitempty" validate:"omitempty"`
+	HTTPHeaderDoNotTrack     string                     `schema:"HttpHeaderDoNotTrack,omitempty" validate:"omitempty"`
+	HTTPHeaderHost           string                     `schema:"HttpHeaderHost,omitempty" validate:"omitempty"`
+	HTTPHeaderReferrer       string                     `schema:"HttpHeaderReferrer,omitempty" validate:"omitempty"`
+	HTTPHeaderUserAgent      string                     `schema:"HttpHeaderUserAgent,omitempty" validate:"omitempty"`
+	HTTPHeaderKeepAlive      string                     `schema:"HttpHeaderKeepAlive,omitempty" validate:"omitempty"`
+	HTTPHeaderUaCpu          string                     `schema:"HttpHeaderUaCpu,omitempty" validate:"omitempty"`
+	HTTPHeaderVia            string                     `schema:"HttpHeaderVia,omitempty" validate:"omitempty"`
+	HTTPHeaderXForwardedFor  string                     `schema:"HttpHeaderXForwardedFor,omitempty" validate:"omitempty"`
+	HTTPHeaderOther          string                     `schema:"HttpHeaderOther,omitempty" validate:"omitempty"`
+	CustomerIp               string                     `schema:"CustomerIp,omitempty" validate:"omitempty"`
+	IMEI                     string                     `schema:"IMEI,omitempty" validate:"omitempty"`
+	DeviceInfo               string                     `schema:"DeviceInfo,omitempty" validate:"omitempty"`
+	CustomerOrderDate        string                     `schema:"CustomerOrderDate,omitempty" validate:"required,len=8"` //YYYYMMDD
+	CustomerName             string                     `schema:"CustomerName,omitempty" validate:"required"`            // kanzi
+	CustomerNameKana         string                     `schema:"CustomerNameKana,omitempty" validate:"required"`        // kana
+	CustomerZipCode          string                     `schema:"CustomerZipCode,omitempty" validate:"required,len=7"`
+	CustomerAddress          string                     `schema:"CustomerAddress,omitempty" validate:"required"`
+	CustomerCompanyName      string                     `schema:"CustomerCompanyName,omitempty" validate:"omitempty"`
+	CustomerDepartmentName   string                     `schema:"CustomerDepartmentName,omitempty" validate:"omitempty"`
+	CustomerTel1             string                     `schema:"CustomerTel1,omitempty" validate:"required"`
+	CustomerTel2             string                     `schema:"CustomerTel2,omitempty" validate:"omitempty"`
+	CustomerEmail1           string                     `schema:"CustomerEmail1,omitempty" validate:"required,email"`
+	CustomerEmail2           string                     `schema:"CustomerEmail2,omitempty" validate:"omitempty,email"`
+	CustomerPaymentType      PostpayCustomerPaymentType `schema:"CustomerPaymentType,omitempty" validate:"required,numeric"`
+	CustomerSex              PostpayCustomerSex         `schema:"CustomerSex,omitempty" validate:"omitempty,numeric"`
+	CustomerBirthday         string                     `schema:"CustomerBirthday,omitempty" validate:"omitempty,len=8"`
+	CustomerRegistDate       string                     `schema:"CustomerRegistDate,omitempty" validate:"omitempty,len=8"`
+	CustomerBuyCount         int                        `schema:"CustomerBuyCount,omitempty" validate:"omitempty,numeric"`
+	CustomerBuyAmountTotal   int                        `schema:"CustomerBuyAmountTotal,omitempty" validate:"omitempty,numeric"`
+	CustomerID               string                     `schema:"CustomerID,omitempty" validate:"omitempty,alphanum"`
+	DeliveryName             string                     `schema:"DeliveryName,omitempty" validate:"omitempty"`
+	DeliveryNameKana         string                     `schema:"DeliveryNameKana,omitempty" validate:"omitempty"`
+	DeliveryZipCode          string                     `schema:"DeliveryZipCode,omitempty" validate:"omitempty,len=7"`
+	DeliveryAddress          string                     `schema:"DeliveryAddress,omitempty" validate:"omitempty"`
+	DeliveryCompanyName      string                     `schema:"DeliveryCompanyName,omitempty" validate:"omitempty"`
+	DeliveryDepartmentName   string                     `schema:"DeliveryDepartmentName,omitempty" validate:"omitempty"`
+	DeliveryTel              string                     `schema:"DeliveryTel,omitempty" validate:"omitempty"`
+	DetailName               string                     `schema:"DetailName,omitempty" validate:"omitempty"`
+	DetailPrice              int                        `schema:"DetailPrice,omitempty" validate:"omitempty,numeric"`
+	DetailQuantity           int                        `schema:"DetailQuantity,omitempty" validate:"omitempty,numeric"`
+	DetailBrand              string                     `schema:"DetailBrand,omitempty" validate:"omitempty"`
+	DetailCategory           string                     `schema:"DetailCategory,omitempty" validate:"omitempty"`
+	ClientField1             string                     `schema:"ClientField1,omitempty" validate:"omitempty"`
+	ClientField2             string                     `schema:"ClientField2,omitempty" validate:"omitempty"`
+	ClientField3             string                     `schema:"ClientField3,omitempty" validate:"omitempty"`
+}
+
+func (r *PostpayChangeTranRequest) Validate() error {
+	return validate.Struct(r)
+}
+
+type PostpayChangeTranResponse struct {
+	OrderID string           `schema:"OrderID,omitempty"`
+	Status  TradeMultiStatus `schema:"Status,omitempty"`
+	ErrCode string           `schema:"ErrCode,omitempty"`
+	ErrInfo string           `schema:"ErrInfo,omitempty"`
+}
+
+func (cli *Client) PostpayChangeTran(
+	req *PostpayChangeTranRequest,
+) (*PostpayChangeTranResponse, error) {
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+	res := &PostpayChangeTranResponse{}
+	_, err := cli.do(postpayChangeTranPath, req, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
 type PostpayCancelTranRequest struct {
 	AccessID   string `schema:"AccessID" validate:"required"`
 	AccessPass string `schema:"AccessPass" validate:"required"`
