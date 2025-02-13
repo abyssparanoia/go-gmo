c9084c1d312ffd752d8db6c2edd00fddef92e5a5
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2025-02-13 12:49:11 +0200

    feat: impl 3dsecure tran2

--- payment/tran.go
+++ payment/tran.go
@@ -11,6 +11,7 @@ type EntryTranRequest struct {
 	ItemCode string `schema:"ItemCode,omitempty"`
 	Amount   int    `schema:"Amount,omitempty"`
 	Tax      int    `schema:"Tax,omitempty"`
+	TDFlag   TDFlag `schema:"TdFlag,omitempty"`
 }
 
 // Validate ... validate
@@ -99,6 +100,48 @@ type ExecTranResponse struct {
 	ErrInfo     string `schema:"ErrInfo,omitempty"`
 }
 
+type ExecTranWith3DSecureRequest struct {
+	AccessID     string `schema:"AccessID" validate:"required"`
+	AccessPass   string `schema:"AccessPass" validate:"required"`
+	OrderID      string `schema:"OrderID" validate:"required,lte=27"`
+	Method       string `schema:"Method,omitempty"`
+	PayTimes     int    `schema:"PayTimes"`
+	MemberID     string `schema:"MemberID" validate:"required"`
+	SeqMode      string `schema:"SeqMode" validate:"required,len=1"`
+	CardSeq      int    `schema:"CardSeq" validate:"lte=9999"`
+	CardPass     string `schema:"CardPass"`
+	SecurityCode string `schema:"SecurityCode"`
+	ClientField1 string `schema:"ClientField1,omitempty"`
+	ClientField2 string `schema:"ClientField2,omitempty"`
+	ClientField3 string `schema:"ClientField3,omitempty"`
+
+	// parameters for 3D Secure
+	RetURL       string                  `schema:"RetURL,omitempty" validate:"required"`
+	CallbackType SecureTran2CallbackType `schema:"CallbackType,omitempty"`
+}
+
+func (r *ExecTranWith3DSecureRequest) Validate() error {
+	return validate.Struct(r)
+}
+
+type ExecTranWith3DSecureResponse struct {
+	ACS         string `schema:"ACS"`
+	OrderID     string `schema:"OrderID"`
+	Forward     string `schema:"forward"`
+	Method      string `schema:"Method"`
+	PayTimes    string `schema:"PayTimes"`
+	Approve     string `schema:"Approve"`
+	TranID      string `schema:"TranID"`
+	TranDate    string `schema:"TranDate"`
+	CheckString string `schema:"CheckString"`
+	ACSUrl      string `schema:"ACSUrl"`
+	PaReq       string `schema:"PaReq"`
+	MD          string `schema:"MD"`
+	RedirectURL string `schema:"RedirectURL"`
+	ErrCode     string `schema:"ErrCode,omitempty"`
+	ErrInfo     string `schema:"ErrInfo,omitempty"`
+}
+
 // ExecTran ... exec tran with member id
 func (cli *Client) ExecTran(
 	req *ExecTranRequest,
@@ -122,6 +165,20 @@ func (cli *Client) ExecTranWithToken(
 		return nil, err
 	}
 	res := &ExecTranResponse{}
+	_, err := cli.do(execTranPath, req, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (cli *Client) ExecTranWith3DSecure(
+	req *ExecTranWith3DSecureRequest,
+) (*ExecTranWith3DSecureResponse, error) {
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+	res := &ExecTranWith3DSecureResponse{}
 	_, err := cli.do(execTranPath, req, res)
 	if err != nil {
 		return nil, err
@@ -207,6 +264,44 @@ func (cli *Client) ChangeTran(
 	}
 	res := &ChangeTranResponse{}
 	_, err := cli.do(changeTranPath, req, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+type SecureTran2Request struct {
+	AccessID   string `schema:"AccessID" validate:"required"`
+	AccessPass string `schema:"AccessPass" validate:"required"`
+}
+
+// Validate ... validate
+func (r *SecureTran2Request) Validate() error {
+	return validate.Struct(r)
+}
+
+type SecureTran2Response struct {
+	OrderID      string `schema:"OrderID,omitempty"`
+	Forward      string `schema:"Forward,omitempty"`
+	Method       string `schema:"Method,omitempty"`
+	PayTimes     string `schema:"PayTimes,omitempty"`
+	Approve      string `schema:"Approve,omitempty"`
+	TranID       string `schema:"TranID,omitempty"`
+	TranDate     string `schema:"TranDate,omitempty"`
+	CheckString  string `schema:"CheckString,omitempty"`
+	ClientField1 string `schema:"ClientField1,omitempty"`
+	ClientField2 string `schema:"ClientField2,omitempty"`
+	ClientField3 string `schema:"ClientField3,omitempty"`
+}
+
+func (cli *Client) SecureTran2(
+	req *SecureTran2Request,
+) (*SecureTran2Response, error) {
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+	res := &SecureTran2Response{}
+	_, err := cli.do(secureTran2Path, req, res)
 	if err != nil {
 		return nil, err
 	}
